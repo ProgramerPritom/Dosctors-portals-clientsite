c7bea51ce1675205839540440a3bfea74cde6c56
ProgramerPritom <badruzzamanpritom@gmail.com>
2022-06-30 19:57:33 +0600

    express api added on services from database

--- src/Pages/Appointment/AppointmentDetails.js
+++ src/Pages/Appointment/AppointmentDetails.js
@@ -9,7 +9,7 @@ const AppointmentDetails = ({date}) => {
     const [treatment,setTreatment] = useState(null);
 
     useEffect( ()=>{
-        fetch('service.json')
+        fetch('http://localhost:5000/service')
         .then(res => res.json())
         .then(data => setServices(data));
     },[])
